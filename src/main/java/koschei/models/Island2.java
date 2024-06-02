61048125096d206de476b447eb905a59b7e76229
Xamz <bugi@gmail.com>
2024-06-02 15:40:57 +0300

    Init commit

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,10 +1,14 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
 public class Island2 {
 
     private Wood3 wood;
 
-    public Island2(Wood3 wood) {
+    public  Island2(Wood3 wood) {
         this.wood = wood;
     }
 
